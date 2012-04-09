b424b3dc8f229298d72e7e03c159aff12115e658
giginet <giginet.net@gmail.com>
2012-04-09 23:02:54 +0900

    Create Fortress

---
+++ Projectfiles/Model/Fortress.h
@@ -0,0 +1,13 @@
+//
+//  Fortress.h
+//  Fortress
+//
+//  Created by  on 2012/4/9.
+//  Copyright (c) 2012 Kawaz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Fortress : NSObject
+
+@end
---
+++ Projectfiles/Model/Fortress.m
@@ -0,0 +1,13 @@
+//
+//  Fortress.m
+//  Fortress
+//
+//  Created by  on 2012/4/9.
+//  Copyright (c) 2012 Kawaz. All rights reserved.
+//
+
+#import "Fortress.h"
+
+@implementation Fortress
+
+@end
